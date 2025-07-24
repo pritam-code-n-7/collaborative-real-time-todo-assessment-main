7a0a64204161f1c8eb839c6fe48bdb5fe598b7da
Pritam Nandy <pritamimca1003@gmail.com>
2025-07-24 21:23:25 +0530

    work

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io"
 
 import { dbConnect } from "./config/db.js";
 import userRoutes from "./routes/user.route.js"
 import todoRoutes from "./routes/todo.route.js"
+import { setupSocket } from "./socket.js";
 
 // express instance
 const app = express();
+const server = createServer(app)
+const io = new Server(server, {
+    cors:{
+        origin: process.env.FE_URL
+    }
+})
 
 // db connection
 dbConnect();
@@ -20,10 +29,13 @@ app.use(express.urlencoded({extended: true}))
 app.use('/api/v1', userRoutes)
 app.use('/api/v1', todoRoutes)
 
+// socket setup
+setupSocket(io)
+
 
 // run the server
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`server running on http://localhost:${port}`);
 })
